92c003a4285035d577198732ac6716945497d839
ankitad08 <ankitadafare2004@gmail.com>
2024-04-03 20:45:31 +0530

    Practical Completed Successfully

---
+++ PracticalNo10.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class PracticalNo10 {
+    public static void main(String[] args) {
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        int sum = numbers.stream()
+                         .mapToInt(Integer::intValue)
+                         .sum();
+
+        System.out.println("Sum of numbers: " + sum);
+    }
+}
